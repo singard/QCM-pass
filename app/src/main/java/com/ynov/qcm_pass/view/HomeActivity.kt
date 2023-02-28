138cc573d96db3ab45c00205bdfdab4a3879f3d9
singard <mathieugonza3@gmail.com>
2023-02-28 19:39:37 +0100

    button main ok

--- app/src/main/java/com/ynov/qcm_pass/view/HomeActivity.kt
+++ app/src/main/java/com/ynov/qcm_pass/view/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.ynov.qcm_pass.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.ynov.qcm_pass.R
 
--- app/src/main/java/com/ynov/qcm_pass/view/MainActivity.kt
+++ app/src/main/java/com/ynov/qcm_pass/view/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ynov.qcm_pass.view
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import com.ynov.qcm_pass.R
 import com.ynov.qcm_pass.databinding.ActivityMainBinding
 
@@ -16,10 +18,16 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val button = findViewById<Button>(R.id.buttonMain)
 
+        button.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
         }
     }

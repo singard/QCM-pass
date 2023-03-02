64b783c2d5f5900e7c6508a1f7c50478dacc32e8
singard <mathieugonza3@gmail.com>
2023-03-02 11:49:38 +0100

    toolbar

--- app/src/main/java/com/ynov/qcm_pass/view/ParametersActivity.kt
+++ app/src/main/java/com/ynov/qcm_pass/view/ParametersActivity.kt
@@ -1,25 +1,19 @@
 package com.ynov.qcm_pass.view
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import com.ynov.qcm_pass.R
 import com.ynov.qcm_pass.onglet.ViewPagerAdapter
 
-class HomeActivity : AppCompatActivity() {
+
+
+class ParametersActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-
-        val viewPager = findViewById<ViewPager>(R.id.view_pager_main)
-        viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
-
-        val tabLayout = findViewById<TabLayout>(R.id.tab_layout_main)
-        tabLayout.setupWithViewPager(viewPager)
+        setContentView(R.layout.activity_parameters)
 
         val toolBar = ToolbarFragment()
         supportFragmentManager.beginTransaction().replace(R.id.container, toolBar).commit()
